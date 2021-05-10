f166494577c2b232ef779723f6438291a4f96931
KovalivSviatoslav <kovaliv2195@gmail.com>
2021-05-10 16:27:48 +0300

    Feat(swagger): add swagger.json and set swagger settings

--- src/__init__.py
+++ src/__init__.py
@@ -3,7 +3,7 @@ from flask import Flask
 from flask_migrate import Migrate
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_swagger_ui import get_swaggerui_blueprint
 
 app = Flask(__name__)
 app_config = app.config.from_object(config.Config)
@@ -11,5 +11,15 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 api = Api(app)
 
-from . import routes, models
+SWAGGER_URL = '/swagger'
+API_URL = '/static/swagger.json'
+SWAGGER_BLUEPRINT = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        'app_name': 'Flask tutorial'
+    }
+)
+app.register_blueprint(SWAGGER_BLUEPRINT, url_prefix=SWAGGER_URL)
 
+from . import routes, models
