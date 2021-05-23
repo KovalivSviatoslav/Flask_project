b5c9d248d66c602f186668dc0f707782691bc976
KovalivSviatoslav <kovaliv2195@gmail.com>
2021-05-23 16:09:55 +0300

    Feat: add psycopg2 and change config.py

--- config.py
+++ config.py
@@ -1,9 +1,14 @@
+import os
 import pathlib
 
 BASE_DIR = pathlib.Path(__file__).parent
 
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + str(BASE_DIR / "data" / "db.sqlite3")
+    postgres_uri = os.getenv("DATABASE_URL")  # or other relevant config var
+    if postgres_uri.startswith("postgres://"):
+        postgres_uri = postgres_uri.replace("postgres://", "postgresql://", 1)
+
+    SQLALCHEMY_DATABASE_URI = postgres_uri or 'sqlite:///' + str(BASE_DIR / "data" / "db.sqlite3")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = 'some_secret_key'
